83564e6bd5dcfdfa3f3fda4d90174d59a088499c
hhasona <hhasona@gmail.com>
2022-11-12 09:35:03 +0200

    add: toc & cfd components

---
+++ src/components/Cfd.js
@@ -0,0 +1,7 @@
+import React from "react"
+import './Cfd.css'
+function ChosenFilmDetails() {
+  return <div className="cfd_container"></div>
+}
+
+export default ChosenFilmDetails
---
+++ src/components/Toc.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react"
+import "./Toc.css"
+function TableOfContent() {
+  const swapiApi = process.env.REACT_APP_SWAPI_API
+  return <div className="toc_container"></div>
+}
+
+export default TableOfContent
