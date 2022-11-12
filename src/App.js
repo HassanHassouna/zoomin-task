34a0bf9c016d19e0ca6c366dc75941779ba90613
hhasona <hhasona@gmail.com>
2022-11-12 10:48:23 +0200

    add: film details functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React, { useState } from "react"
 import "./App.css"
 import ChosenFilmDetails from "./components/Cfd"
 import TableOfContent from "./components/Toc"
 
 function App() {
+  const [chosenFilm, setChosenFilm] = useState(" No Film Chosen")
+  console.log("chosenFilm", chosenFilm)
   return (
     <div className="App">
       {/* TOC */}
       <div className="app_container">
-        <TableOfContent />
+        <TableOfContent chosenFilm={chosenFilm} setChosenFilm={setChosenFilm} />
         {/* Chosen Film Details */}
-        <ChosenFilmDetails />
+        <ChosenFilmDetails chosenFilm={chosenFilm} />
       </div>
     </div>
   )
--- src/components/Cfd.js
+++ src/components/Cfd.js
@@ -1,7 +1,28 @@
 import React from "react"
-import './Cfd.css'
-function ChosenFilmDetails() {
-  return <div className="cfd_container"></div>
+import "./Cfd.css"
+
+function ChosenFilmDetails({ chosenFilm }) {
+  return (
+    <div className="cfd_container">
+      {chosenFilm.title ? (
+        <div className="cfd_film_details">
+          <h1>{chosenFilm.title}</h1>
+          <p>{chosenFilm.opening_crawl}</p>
+        </div>
+      ) : (
+        <h1
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          {chosenFilm}
+        </h1>
+      )}
+    </div>
+  )
 }
 
 export default ChosenFilmDetails
