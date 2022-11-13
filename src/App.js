f57921ab024ae27d599051d6126fad7fb0d48ed6
hhasona <hhasona@gmail.com>
2022-11-13 10:12:08 +0200

    remove: console log and unnecessary staff

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import TableOfContent from "./components/Toc"
 
 function App() {
   const [chosenFilm, setChosenFilm] = useState(" No Film Chosen")
-  console.log("chosenFilm", chosenFilm)
   return (
     <div className="App">
-      {/* TOC */}
       <div className="app_container">
         <TableOfContent chosenFilm={chosenFilm} setChosenFilm={setChosenFilm} />
-        {/* Chosen Film Details */}
         <ChosenFilmDetails chosenFilm={chosenFilm} />
       </div>
     </div>
